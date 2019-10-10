ximliu/v2ray-core

Add track ip config

app/stats/command/command.pb.go
@@ -240,6 +240,53 @@ func (m *QueryStatsResponse) GetStat() []*Stat {
 	return nil
 }
 
+type GetUserIPStatsResponse struct {
+	Name                 string   `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	Value                string   `protobuf:"bytes,2,opt,name=value,proto3" json:"value,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *GetUserIPStatsResponse) Reset()         { *m = GetUserIPStatsResponse{} }
+func (m *GetUserIPStatsResponse) String() string { return proto.CompactTextString(m) }
+func (*GetUserIPStatsResponse) ProtoMessage()    {}
+func (*GetUserIPStatsResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_c902411c4948f26b, []int{5}
+}
+
+func (m *GetUserIPStatsResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_GetUserIPStatsResponse.Unmarshal(m, b)
+}
+func (m *GetUserIPStatsResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_GetUserIPStatsResponse.Marshal(b, m, deterministic)
+}
+func (m *GetUserIPStatsResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GetUserIPStatsResponse.Merge(m, src)
+}
+func (m *GetUserIPStatsResponse) XXX_Size() int {
+	return xxx_messageInfo_GetUserIPStatsResponse.Size(m)
+}
+func (m *GetUserIPStatsResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_GetUserIPStatsResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_GetUserIPStatsResponse proto.InternalMessageInfo
+
+func (m *GetUserIPStatsResponse) GetName() string {
+	if m != nil {
+		return m.Name
+	}
+	return ""
+}
+
+func (m *GetUserIPStatsResponse) GetValue() string {
+	if m != nil {
+		return m.Value
+	}
+	return ""
+}
+
 type Config struct {
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
@@ -250,7 +297,7 @@ func (m *Config) Reset()         { *m = Config{} }
 func (m *Config) String() string { return proto.CompactTextString(m) }
 func (*Config) ProtoMessage()    {}
 func (*Config) Descriptor() ([]byte, []int) {
-	return fileDescriptor_c902411c4948f26b, []int{5}
+	return fileDescriptor_c902411c4948f26b, []int{6}
 }
 
 func (m *Config) XXX_Unmarshal(b []byte) error {
@@ -277,6 +324,7 @@ func init() {
 	proto.RegisterType((*GetStatsResponse)(nil), "v2ray.core.app.stats.command.GetStatsResponse")
 	proto.RegisterType((*QueryStatsRequest)(nil), "v2ray.core.app.stats.command.QueryStatsRequest")
 	proto.RegisterType((*QueryStatsResponse)(nil), "v2ray.core.app.stats.command.QueryStatsResponse")
+	proto.RegisterType((*GetUserIPStatsResponse)(nil), "v2ray.core.app.stats.command.GetUserIPStatsResponse")
 	proto.RegisterType((*Config)(nil), "v2ray.core.app.stats.command.Config")
 }
 
@@ -285,28 +333,30 @@ func init() {
 }
 
 var fileDescriptor_c902411c4948f26b = []byte{
-	// 321 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x94, 0x92, 0xb1, 0x4e, 0xc3, 0x30,
-	0x10, 0x86, 0x49, 0x5b, 0xda, 0x72, 0x20, 0x01, 0x16, 0x43, 0x55, 0x75, 0x88, 0x3c, 0x75, 0xc1,
-	0xa9, 0x82, 0xc4, 0xc2, 0x04, 0x19, 0x90, 0x50, 0x07, 0x70, 0x25, 0x06, 0x36, 0x13, 0x0e, 0x54,
-	0x41, 0x62, 0xd7, 0x76, 0x22, 0xe5, 0x95, 0x78, 0x38, 0x9e, 0x01, 0xc5, 0x49, 0x54, 0xa0, 0x6a,
-	0x54, 0xa6, 0xdc, 0xc5, 0xff, 0x77, 0xf7, 0xdf, 0xd9, 0xc0, 0xf2, 0x50, 0x8b, 0x82, 0xc5, 0x32,
-	0x09, 0x62, 0xa9, 0x31, 0x10, 0x4a, 0x05, 0xc6, 0x0a, 0x6b, 0x82, 0x58, 0x26, 0x89, 0x48, 0x5f,
-	0x9a, 0x2f, 0x53, 0x5a, 0x5a, 0x49, 0x26, 0x8d, 0x5e, 0x23, 0x13, 0x4a, 0x31, 0xa7, 0x65, 0xb5,
-	0x86, 0x5e, 0xc1, 0xf1, 0x2d, 0xda, 0x45, 0xf9, 0x8f, 0xe3, 0x2a, 0x43, 0x63, 0x09, 0x81, 0x5e,
-	0x2a, 0x12, 0x1c, 0x79, 0xbe, 0x37, 0x3d, 0xe0, 0x2e, 0x26, 0x67, 0xb0, 0xaf, 0xd1, 0xa0, 0x1d,
-	0x75, 0x7c, 0x6f, 0x3a, 0xe4, 0x55, 0x42, 0x67, 0xd0, 0x2b, 0xc9, 0x6d, 0x44, 0x2e, 0x3e, 0x32,
-	0x74, 0x44, 0x97, 0x57, 0x09, 0xbd, 0x83, 0x93, 0x75, 0x3b, 0xa3, 0x64, 0x6a, 0x90, 0x5c, 0x42,
-	0xaf, 0xf4, 0xe4, 0xe8, 0xc3, 0x90, 0xb2, 0x36, 0xbf, 0xac, 0x44, 0xb9, 0xd3, 0xd3, 0x08, 0x4e,
-	0x1f, 0x32, 0xd4, 0xc5, 0x2f, 0xf3, 0x23, 0x18, 0x28, 0x61, 0x2d, 0xea, 0xb4, 0x76, 0xd3, 0xa4,
-	0x5b, 0x46, 0x98, 0x03, 0xf9, 0x59, 0x64, 0xc3, 0x52, 0xf7, 0x5f, 0x96, 0x86, 0xd0, 0x8f, 0x64,
-	0xfa, 0xba, 0x7c, 0x0b, 0xbf, 0x3c, 0x38, 0x72, 0x35, 0x17, 0xa8, 0xf3, 0x65, 0x8c, 0xe4, 0x1d,
-	0x86, 0xcd, 0xe4, 0xe4, 0xbc, 0xbd, 0xe0, 0x9f, 0x0b, 0x19, 0xb3, 0x5d, 0xe5, 0x95, 0x7b, 0xba,
-	0x47, 0x56, 0x00, 0xeb, 0xa9, 0x48, 0xd0, 0xce, 0x6f, 0x2c, 0x71, 0x3c, 0xdb, 0x1d, 0x68, 0x5a,
-	0xde, 0xcc, 0xc1, 0x8f, 0x65, 0xd2, 0x0a, 0xde, 0x7b, 0x4f, 0x83, 0x3a, 0xfc, 0xec, 0x4c, 0x1e,
-	0x43, 0x2e, 0x0a, 0x16, 0x95, 0xca, 0x6b, 0xa5, 0xdc, 0x16, 0x0d, 0x8b, 0xaa, 0xe3, 0xe7, 0xbe,
-	0x7b, 0xbb, 0x17, 0xdf, 0x01, 0x00, 0x00, 0xff, 0xff, 0xcc, 0x9e, 0xb8, 0xeb, 0xed, 0x02, 0x00,
-	0x00,
+	// 359 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x94, 0x93, 0xcf, 0x4e, 0xea, 0x40,
+	0x14, 0xc6, 0x29, 0x70, 0xf9, 0x73, 0xee, 0xcd, 0xbd, 0xd7, 0x89, 0x31, 0x84, 0xb0, 0x20, 0xb3,
+	0x62, 0xe3, 0x94, 0x54, 0xe3, 0xc6, 0x95, 0x74, 0x41, 0x34, 0x2c, 0xb0, 0x44, 0x17, 0xee, 0xc6,
+	0x7a, 0x34, 0x44, 0xdb, 0x19, 0x66, 0x06, 0x0c, 0xaf, 0xe4, 0x8b, 0xf9, 0x1a, 0xa6, 0x53, 0x2a,
+	0x22, 0x52, 0x60, 0xd5, 0x39, 0xed, 0xf7, 0x9d, 0xf9, 0xcd, 0x77, 0x3a, 0xc0, 0x66, 0x9e, 0xe2,
+	0x73, 0x16, 0x8a, 0xc8, 0x0d, 0x85, 0x42, 0x97, 0x4b, 0xe9, 0x6a, 0xc3, 0x8d, 0x76, 0x43, 0x11,
+	0x45, 0x3c, 0x7e, 0xc8, 0x9e, 0x4c, 0x2a, 0x61, 0x04, 0x69, 0x65, 0x7a, 0x85, 0x8c, 0x4b, 0xc9,
+	0xac, 0x96, 0x2d, 0x34, 0xf4, 0x1c, 0xfe, 0xf5, 0xd1, 0x8c, 0x92, 0x77, 0x01, 0x4e, 0xa6, 0xa8,
+	0x0d, 0x21, 0x50, 0x8e, 0x79, 0x84, 0x0d, 0xa7, 0xed, 0x74, 0xea, 0x81, 0x5d, 0x93, 0x43, 0xf8,
+	0xa5, 0x50, 0xa3, 0x69, 0x14, 0xdb, 0x4e, 0xa7, 0x16, 0xa4, 0x05, 0xed, 0x42, 0x39, 0x71, 0x6e,
+	0x72, 0xcc, 0xf8, 0xcb, 0x14, 0xad, 0xa3, 0x14, 0xa4, 0x05, 0xbd, 0x82, 0xff, 0xcb, 0xed, 0xb4,
+	0x14, 0xb1, 0x46, 0x72, 0x06, 0xe5, 0x84, 0xc9, 0xba, 0x7f, 0x7b, 0x94, 0xe5, 0xf1, 0xb2, 0xc4,
+	0x1a, 0x58, 0x3d, 0xf5, 0xe1, 0xe0, 0x7a, 0x8a, 0x6a, 0xbe, 0x02, 0xdf, 0x80, 0xaa, 0xe4, 0xc6,
+	0xa0, 0x8a, 0x17, 0x34, 0x59, 0xb9, 0xe1, 0x08, 0x03, 0x20, 0x5f, 0x9b, 0xac, 0x21, 0x95, 0xf6,
+	0x42, 0xea, 0xc1, 0x51, 0x1f, 0xcd, 0x8d, 0x46, 0x75, 0x39, 0x5c, 0xed, 0xb8, 0x35, 0xa2, 0x7a,
+	0x16, 0x51, 0x0d, 0x2a, 0xbe, 0x88, 0x1f, 0xc7, 0x4f, 0xde, 0x7b, 0x11, 0xfe, 0xd8, 0x2e, 0x23,
+	0x54, 0xb3, 0x71, 0x88, 0xe4, 0x19, 0x6a, 0x59, 0x7a, 0xe4, 0x38, 0x1f, 0xea, 0xdb, 0x50, 0x9b,
+	0x6c, 0x57, 0x79, 0xca, 0x4b, 0x0b, 0x64, 0x02, 0xb0, 0x4c, 0x86, 0xb8, 0xf9, 0xfe, 0xb5, 0x41,
+	0x34, 0xbb, 0xbb, 0x1b, 0x3e, 0xb7, 0x7c, 0x85, 0xbf, 0xab, 0xf1, 0xed, 0x7b, 0xca, 0xd3, 0xad,
+	0xf2, 0x1f, 0x66, 0x43, 0x0b, 0xbd, 0x01, 0xb4, 0x43, 0x11, 0xe5, 0x9a, 0x87, 0xce, 0x5d, 0x75,
+	0xb1, 0x7c, 0x2b, 0xb6, 0x6e, 0xbd, 0x80, 0xcf, 0x99, 0x9f, 0x28, 0x2f, 0xa4, 0xb4, 0xbf, 0x80,
+	0x66, 0x7e, 0xfa, 0xf9, 0xbe, 0x62, 0x2f, 0xde, 0xc9, 0x47, 0x00, 0x00, 0x00, 0xff, 0xff, 0xd1,
+	0xc4, 0x60, 0x2a, 0xaa, 0x03, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -323,6 +373,7 @@ const _ = grpc.SupportPackageIsVersion4
 type StatsServiceClient interface {
 	GetStats(ctx context.Context, in *GetStatsRequest, opts ...grpc.CallOption) (*GetStatsResponse, error)
 	QueryStats(ctx context.Context, in *QueryStatsRequest, opts ...grpc.CallOption) (*QueryStatsResponse, error)
+	GetUserIPStats(ctx context.Context, in *GetStatsRequest, opts ...grpc.CallOption) (*GetUserIPStatsResponse, error)
 }
 
 type statsServiceClient struct {
@@ -351,10 +402,20 @@ func (c *statsServiceClient) QueryStats(ctx context.Context, in *QueryStatsReque
 	return out, nil
 }
 
+func (c *statsServiceClient) GetUserIPStats(ctx context.Context, in *GetStatsRequest, opts ...grpc.CallOption) (*GetUserIPStatsResponse, error) {
+	out := new(GetUserIPStatsResponse)
+	err := c.cc.Invoke(ctx, "/v2ray.core.app.stats.command.StatsService/GetUserIPStats", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // StatsServiceServer is the server API for StatsService service.
 type StatsServiceServer interface {
 	GetStats(context.Context, *GetStatsRequest) (*GetStatsResponse, error)
 	QueryStats(context.Context, *QueryStatsRequest) (*QueryStatsResponse, error)
+	GetUserIPStats(context.Context, *GetStatsRequest) (*GetUserIPStatsResponse, error)
 }
 
 func RegisterStatsServiceServer(s *grpc.Server, srv StatsServiceServer) {
@@ -397,6 +458,24 @@ func _StatsService_QueryStats_Handler(srv interface{}, ctx context.Context, dec 
 	return interceptor(ctx, in, info, handler)
 }
 
+func _StatsService_GetUserIPStats_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetStatsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(StatsServiceServer).GetUserIPStats(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/v2ray.core.app.stats.command.StatsService/GetUserIPStats",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(StatsServiceServer).GetUserIPStats(ctx, req.(*GetStatsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _StatsService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "v2ray.core.app.stats.command.StatsService",
 	HandlerType: (*StatsServiceServer)(nil),
@@ -408,6 +487,10 @@ var _StatsService_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "QueryStats",
 			Handler:    _StatsService_QueryStats_Handler,
+		},
+		{
+			MethodName: "GetUserIPStats",
+			Handler:    _StatsService_GetUserIPStats_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},

supersongssr/v2ray-core-srp-fork

Conflicts:
	app/stats/command/command.go
	app/stats/command/command.pb.go
	app/stats/command/command.proto

app/stats/command/command.pb.go
@@ -1,6 +1,3 @@
-// Code generated by protoc-gen-go. DO NOT EDIT.
-// source: v2ray.com/core/app/stats/command/command.proto
-
 package command
 
 import (
@@ -387,6 +384,53 @@ func (m *SysStatsResponse) GetUptime() uint32 {
 	return 0
 }
 
+type GetUserIPStatsResponse struct {
+	Name                 string   `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	Value                string   `protobuf:"bytes,2,opt,name=value,proto3" json:"value,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *GetUserIPStatsResponse) Reset()         { *m = GetUserIPStatsResponse{} }
+func (m *GetUserIPStatsResponse) String() string { return proto.CompactTextString(m) }
+func (*GetUserIPStatsResponse) ProtoMessage()    {}
+func (*GetUserIPStatsResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_c902411c4948f26b, []int{7}
+}
+
+func (m *GetUserIPStatsResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_GetUserIPStatsResponse.Unmarshal(m, b)
+}
+func (m *GetUserIPStatsResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_GetUserIPStatsResponse.Marshal(b, m, deterministic)
+}
+func (m *GetUserIPStatsResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GetUserIPStatsResponse.Merge(m, src)
+}
+func (m *GetUserIPStatsResponse) XXX_Size() int {
+	return xxx_messageInfo_GetUserIPStatsResponse.Size(m)
+}
+func (m *GetUserIPStatsResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_GetUserIPStatsResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_GetUserIPStatsResponse proto.InternalMessageInfo
+
+func (m *GetUserIPStatsResponse) GetName() string {
+	if m != nil {
+		return m.Name
+	}
+	return ""
+}
+
+func (m *GetUserIPStatsResponse) GetValue() string {
+	if m != nil {
+		return m.Value
+	}
+	return ""
+}
+
 type Config struct {
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
@@ -397,7 +441,7 @@ func (m *Config) Reset()         { *m = Config{} }
 func (m *Config) String() string { return proto.CompactTextString(m) }
 func (*Config) ProtoMessage()    {}
 func (*Config) Descriptor() ([]byte, []int) {
-	return fileDescriptor_c902411c4948f26b, []int{7}
+	return fileDescriptor_c902411c4948f26b, []int{8}
 }
 
 func (m *Config) XXX_Unmarshal(b []byte) error {
@@ -426,6 +470,7 @@ func init() {
 	proto.RegisterType((*QueryStatsResponse)(nil), "v2ray.core.app.stats.command.QueryStatsResponse")
 	proto.RegisterType((*SysStatsRequest)(nil), "v2ray.core.app.stats.command.SysStatsRequest")
 	proto.RegisterType((*SysStatsResponse)(nil), "v2ray.core.app.stats.command.SysStatsResponse")
+	proto.RegisterType((*GetUserIPStatsResponse)(nil), "v2ray.core.app.stats.command.GetUserIPStatsResponse")
 	proto.RegisterType((*Config)(nil), "v2ray.core.app.stats.command.Config")
 }
 
@@ -434,38 +479,40 @@ func init() {
 }
 
 var fileDescriptor_c902411c4948f26b = []byte{
-	// 488 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x94, 0x54, 0x5d, 0x6f, 0xd3, 0x30,
-	0x14, 0x5d, 0xdb, 0xac, 0xed, 0x6e, 0x37, 0xad, 0xb3, 0x10, 0xb2, 0xa6, 0x09, 0x45, 0x7e, 0xea,
-	0x0b, 0xce, 0x54, 0x24, 0x5e, 0x78, 0x1a, 0x91, 0x98, 0x84, 0xca, 0x18, 0x29, 0xf0, 0xc0, 0x9b,
-	0x17, 0x2e, 0x28, 0xd0, 0xc4, 0x9e, 0xed, 0x54, 0xca, 0xdf, 0xe1, 0x91, 0x1f, 0xc5, 0x6f, 0x41,
-	0x76, 0x12, 0xfa, 0x31, 0xad, 0x1b, 0x4f, 0xf5, 0x39, 0xf7, 0x9e, 0xdc, 0x73, 0xed, 0xa3, 0x02,
-	0x5f, 0x4e, 0xb5, 0xa8, 0x78, 0x2a, 0xf3, 0x28, 0x95, 0x1a, 0x23, 0xa1, 0x54, 0x64, 0xac, 0xb0,
-	0x26, 0x4a, 0x65, 0x9e, 0x8b, 0xe2, 0x6b, 0xfb, 0xcb, 0x95, 0x96, 0x56, 0x92, 0xb3, 0xb6, 0x5f,
-	0x23, 0x17, 0x4a, 0x71, 0xdf, 0xcb, 0x9b, 0x1e, 0xf6, 0x0a, 0x8e, 0x2f, 0xd1, 0xce, 0x1d, 0x97,
-	0xe0, 0x6d, 0x89, 0xc6, 0x12, 0x02, 0x41, 0x21, 0x72, 0xa4, 0x9d, 0xb0, 0x33, 0x39, 0x48, 0xfc,
-	0x99, 0x3c, 0x81, 0x7d, 0x8d, 0x06, 0x2d, 0xed, 0x86, 0x9d, 0xc9, 0x30, 0xa9, 0x01, 0x3b, 0x87,
-	0xc0, 0x29, 0xef, 0x53, 0x2c, 0xc5, 0xa2, 0x44, 0xaf, 0xe8, 0x25, 0x35, 0x60, 0x6f, 0x61, 0xbc,
-	0x1a, 0x67, 0x94, 0x2c, 0x0c, 0x92, 0x97, 0x10, 0x38, 0x4f, 0x5e, 0x3d, 0x9a, 0x32, 0xbe, 0xcb,
-	0x2f, 0x77, 0xd2, 0xc4, 0xf7, 0xb3, 0x18, 0x4e, 0x3e, 0x94, 0xa8, 0xab, 0x0d, 0xf3, 0x14, 0x06,
-	0x4a, 0x58, 0x8b, 0xba, 0x68, 0xdc, 0xb4, 0xf0, 0x9e, 0x15, 0x66, 0x40, 0xd6, 0x3f, 0x72, 0xc7,
-	0x52, 0xef, 0xbf, 0x2c, 0x9d, 0xc0, 0xf1, 0xbc, 0x32, 0xeb, 0x86, 0xd8, 0xaf, 0x2e, 0x8c, 0x57,
-	0x5c, 0xf3, 0x7d, 0x06, 0x87, 0x57, 0x65, 0x7e, 0x29, 0xb5, 0x2c, 0x6d, 0x56, 0xd4, 0x17, 0x77,
-	0x94, 0x6c, 0x70, 0xce, 0xaf, 0xc3, 0xb1, 0xf7, 0x7b, 0x94, 0xd4, 0xc0, 0xb1, 0x17, 0x8b, 0x85,
-	0x4c, 0x69, 0x2f, 0xec, 0x4c, 0x82, 0xa4, 0x06, 0xe4, 0x19, 0xc0, 0x47, 0x69, 0xc5, 0xa2, 0x2e,
-	0x05, 0xbe, 0xb4, 0xc6, 0x90, 0x31, 0xf4, 0xe6, 0x95, 0xa1, 0xfb, 0xbe, 0xe0, 0x8e, 0xee, 0x9e,
-	0xde, 0x09, 0x57, 0x33, 0xb4, 0xef, 0xd9, 0x16, 0xba, 0x09, 0x6f, 0x34, 0xa2, 0xa1, 0x83, 0x7a,
-	0x82, 0x07, 0x24, 0x84, 0xd1, 0x2c, 0x5b, 0xe2, 0xfb, 0x9b, 0x1f, 0x98, 0x5a, 0x43, 0x87, 0xbe,
-	0xb6, 0x4e, 0xb9, 0x9d, 0xae, 0x45, 0x69, 0xd0, 0x8f, 0xbd, 0x32, 0xf4, 0xc0, 0xb7, 0x6c, 0x70,
-	0xe4, 0x29, 0xf4, 0x3f, 0x29, 0x9b, 0xe5, 0x48, 0xc1, 0x2f, 0xd5, 0x20, 0x36, 0x84, 0x7e, 0x2c,
-	0x8b, 0x6f, 0xd9, 0xf7, 0xe9, 0x9f, 0x2e, 0x1c, 0xfa, 0xbb, 0x9a, 0xa3, 0x5e, 0x66, 0x29, 0x92,
-	0x9f, 0x30, 0x6c, 0x13, 0x43, 0x9e, 0xef, 0x7e, 0x88, 0xad, 0x20, 0x9f, 0xf2, 0xc7, 0xb6, 0xd7,
-	0xaf, 0xc2, 0xf6, 0xc8, 0x2d, 0xc0, 0x2a, 0x0d, 0x24, 0xda, 0xad, 0xbf, 0x13, 0xbe, 0xd3, 0xf3,
-	0xc7, 0x0b, 0xfe, 0x8d, 0x2c, 0x60, 0xe4, 0x8c, 0x34, 0x09, 0x79, 0x68, 0xc5, 0xad, 0x74, 0x3d,
-	0xb4, 0xe2, 0x76, 0xf0, 0xd8, 0xde, 0xeb, 0x19, 0x84, 0xa9, 0xcc, 0x77, 0xca, 0xae, 0x3b, 0x5f,
-	0x06, 0xcd, 0xf1, 0x77, 0xf7, 0xec, 0xf3, 0x34, 0x11, 0x15, 0x8f, 0x5d, 0xe7, 0x85, 0x52, 0x3e,
-	0xed, 0x86, 0xc7, 0x75, 0xf9, 0xa6, 0xef, 0xff, 0x63, 0x5e, 0xfc, 0x0d, 0x00, 0x00, 0xff, 0xff,
-	0x25, 0xa8, 0x5c, 0x1b, 0x95, 0x04, 0x00, 0x00,
+	// 522 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x94, 0x54, 0x4d, 0x6f, 0x13, 0x31,
+	0x10, 0xcd, 0x57, 0xf3, 0x31, 0x69, 0x69, 0x6a, 0xa1, 0xca, 0xaa, 0x2a, 0x14, 0xf9, 0x94, 0x0b,
+	0x4e, 0x15, 0x10, 0x17, 0x4e, 0x6d, 0x24, 0x22, 0x50, 0x28, 0x61, 0x43, 0x39, 0x70, 0x73, 0x97,
+	0x01, 0x05, 0xb2, 0x6b, 0xd7, 0xf6, 0x06, 0xe5, 0xef, 0x70, 0xe4, 0x0f, 0xf1, 0x77, 0x90, 0xbd,
+	0xbb, 0x24, 0x9b, 0xb6, 0x49, 0x7b, 0x8a, 0xdf, 0xcc, 0x3c, 0xcf, 0x9b, 0xd9, 0xe7, 0x00, 0x5f,
+	0x0c, 0xb4, 0x58, 0xf2, 0x50, 0x46, 0xfd, 0x50, 0x6a, 0xec, 0x0b, 0xa5, 0xfa, 0xc6, 0x0a, 0x6b,
+	0xfa, 0xa1, 0x8c, 0x22, 0x11, 0x7f, 0xcd, 0x7f, 0xb9, 0xd2, 0xd2, 0x4a, 0x72, 0x9a, 0xd7, 0x6b,
+	0xe4, 0x42, 0x29, 0xee, 0x6b, 0x79, 0x56, 0xc3, 0x5e, 0xc3, 0xe1, 0x08, 0xed, 0xd4, 0xc5, 0x02,
+	0xbc, 0x49, 0xd0, 0x58, 0x42, 0xa0, 0x16, 0x8b, 0x08, 0x69, 0xb9, 0x5b, 0xee, 0xb5, 0x02, 0x7f,
+	0x26, 0x4f, 0x61, 0x4f, 0xa3, 0x41, 0x4b, 0x2b, 0xdd, 0x72, 0xaf, 0x19, 0xa4, 0x80, 0x9d, 0x41,
+	0xcd, 0x31, 0xef, 0x63, 0x2c, 0xc4, 0x3c, 0x41, 0xcf, 0xa8, 0x06, 0x29, 0x60, 0xef, 0xa0, 0xb3,
+	0x6a, 0x67, 0x94, 0x8c, 0x0d, 0x92, 0x57, 0x50, 0x73, 0x9a, 0x3c, 0xbb, 0x3d, 0x60, 0x7c, 0x9b,
+	0x5e, 0xee, 0xa8, 0x81, 0xaf, 0x67, 0x43, 0x38, 0xfa, 0x98, 0xa0, 0x5e, 0x16, 0xc4, 0x53, 0x68,
+	0x28, 0x61, 0x2d, 0xea, 0x38, 0x53, 0x93, 0xc3, 0x7b, 0x46, 0x18, 0x03, 0x59, 0xbf, 0xe4, 0x96,
+	0xa4, 0xea, 0xa3, 0x24, 0x1d, 0xc1, 0xe1, 0x74, 0x69, 0xd6, 0x05, 0xb1, 0xdf, 0x15, 0xe8, 0xac,
+	0x62, 0xd9, 0xfd, 0x0c, 0xf6, 0x2f, 0x93, 0x68, 0x24, 0xb5, 0x4c, 0xec, 0x2c, 0x4e, 0x17, 0x77,
+	0x10, 0x14, 0x62, 0x4e, 0xaf, 0xc3, 0x43, 0xaf, 0xf7, 0x20, 0x48, 0x81, 0x8b, 0x9e, 0xcf, 0xe7,
+	0x32, 0xa4, 0xd5, 0x6e, 0xb9, 0x57, 0x0b, 0x52, 0x40, 0x9e, 0x01, 0x7c, 0x92, 0x56, 0xcc, 0xd3,
+	0x54, 0xcd, 0xa7, 0xd6, 0x22, 0xa4, 0x03, 0xd5, 0xe9, 0xd2, 0xd0, 0x3d, 0x9f, 0x70, 0x47, 0xb7,
+	0xa7, 0xf7, 0xc2, 0xe5, 0x0c, 0xad, 0xfb, 0x68, 0x0e, 0x5d, 0x87, 0x37, 0x1a, 0xd1, 0xd0, 0x46,
+	0xda, 0xc1, 0x03, 0xd2, 0x85, 0xf6, 0x78, 0xb6, 0xc0, 0x0f, 0xd7, 0x3f, 0x30, 0xb4, 0x86, 0x36,
+	0x7d, 0x6e, 0x3d, 0xe4, 0x66, 0x9a, 0x88, 0xc4, 0xa0, 0x6f, 0x7b, 0x69, 0x68, 0xcb, 0x97, 0x14,
+	0x62, 0xe4, 0x18, 0xea, 0x57, 0xca, 0xce, 0x22, 0xa4, 0xe0, 0x87, 0xca, 0x10, 0xbb, 0x80, 0xe3,
+	0x11, 0xda, 0x2b, 0x83, 0xfa, 0xed, 0xa4, 0xb8, 0xa9, 0x9d, 0xd6, 0x6a, 0xe5, 0xd6, 0x6a, 0x42,
+	0x7d, 0x28, 0xe3, 0x6f, 0xb3, 0xef, 0x83, 0xbf, 0x55, 0xd8, 0xf7, 0xb7, 0x4c, 0x51, 0x2f, 0x66,
+	0x21, 0x92, 0x9f, 0xd0, 0xcc, 0x5d, 0x47, 0x9e, 0x6f, 0xff, 0x98, 0x1b, 0x8f, 0xe1, 0x84, 0x3f,
+	0xb4, 0x3c, 0xd5, 0xcb, 0x4a, 0xe4, 0x06, 0x60, 0xe5, 0x28, 0xd2, 0xdf, 0xce, 0xbf, 0x65, 0xe0,
+	0x93, 0xb3, 0x87, 0x13, 0xfe, 0xb7, 0x8c, 0xa1, 0xed, 0x84, 0x64, 0x2e, 0xdb, 0x35, 0xe2, 0x86,
+	0x43, 0x77, 0x8d, 0xb8, 0x69, 0x5e, 0x56, 0x22, 0xbf, 0xe0, 0x49, 0xf1, 0x73, 0x3d, 0x76, 0xab,
+	0x2f, 0x77, 0x96, 0xdf, 0xe1, 0x05, 0x56, 0xba, 0x18, 0x43, 0x37, 0x94, 0xd1, 0x56, 0xf2, 0xa4,
+	0xfc, 0xa5, 0x91, 0x1d, 0xff, 0x54, 0x4e, 0x3f, 0x0f, 0x02, 0xb1, 0xe4, 0x43, 0x57, 0x79, 0xae,
+	0x94, 0x7f, 0xaa, 0x86, 0x0f, 0xd3, 0xf4, 0x75, 0xdd, 0xff, 0x41, 0xbe, 0xf8, 0x17, 0x00, 0x00,
+	0xff, 0xff, 0xb6, 0xe7, 0x88, 0x07, 0x52, 0x05, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -483,6 +530,7 @@ type StatsServiceClient interface {
 	GetStats(ctx context.Context, in *GetStatsRequest, opts ...grpc.CallOption) (*GetStatsResponse, error)
 	QueryStats(ctx context.Context, in *QueryStatsRequest, opts ...grpc.CallOption) (*QueryStatsResponse, error)
 	GetSysStats(ctx context.Context, in *SysStatsRequest, opts ...grpc.CallOption) (*SysStatsResponse, error)
+	GetUserIPStats(ctx context.Context, in *GetStatsRequest, opts ...grpc.CallOption) (*GetUserIPStatsResponse, error)
 }
 
 type statsServiceClient struct {
@@ -520,11 +568,21 @@ func (c *statsServiceClient) GetSysStats(ctx context.Context, in *SysStatsReques
 	return out, nil
 }
 
+func (c *statsServiceClient) GetUserIPStats(ctx context.Context, in *GetStatsRequest, opts ...grpc.CallOption) (*GetUserIPStatsResponse, error) {
+	out := new(GetUserIPStatsResponse)
+	err := c.cc.Invoke(ctx, "/v2ray.core.app.stats.command.StatsService/GetUserIPStats", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // StatsServiceServer is the server API for StatsService service.
 type StatsServiceServer interface {
 	GetStats(context.Context, *GetStatsRequest) (*GetStatsResponse, error)
 	QueryStats(context.Context, *QueryStatsRequest) (*QueryStatsResponse, error)
 	GetSysStats(context.Context, *SysStatsRequest) (*SysStatsResponse, error)
+	GetUserIPStats(context.Context, *GetStatsRequest) (*GetUserIPStatsResponse, error)
 }
 
 // UnimplementedStatsServiceServer can be embedded to have forward compatible implementations.
@@ -539,6 +597,9 @@ func (*UnimplementedStatsServiceServer) QueryStats(ctx context.Context, req *Que
 }
 func (*UnimplementedStatsServiceServer) GetSysStats(ctx context.Context, req *SysStatsRequest) (*SysStatsResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetSysStats not implemented")
+}
+func (*UnimplementedStatsServiceServer) GetUserIPStats(ctx context.Context, req *GetStatsRequest) (*GetUserIPStatsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetUserIPStats not implemented")
 }
 
 func RegisterStatsServiceServer(s *grpc.Server, srv StatsServiceServer) {
@@ -599,6 +660,24 @@ func _StatsService_GetSysStats_Handler(srv interface{}, ctx context.Context, dec
 	return interceptor(ctx, in, info, handler)
 }
 
+func _StatsService_GetUserIPStats_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetStatsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(StatsServiceServer).GetUserIPStats(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/v2ray.core.app.stats.command.StatsService/GetUserIPStats",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(StatsServiceServer).GetUserIPStats(ctx, req.(*GetStatsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _StatsService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "v2ray.core.app.stats.command.StatsService",
 	HandlerType: (*StatsServiceServer)(nil),
@@ -614,6 +693,10 @@ var _StatsService_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetSysStats",
 			Handler:    _StatsService_GetSysStats_Handler,
+		},
+		{
+			MethodName: "GetUserIPStats",
+			Handler:    _StatsService_GetUserIPStats_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},